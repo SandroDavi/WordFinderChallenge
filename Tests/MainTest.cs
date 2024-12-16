6b6853d0906fdaceb6273b6ed2a3dd0ac212efd5
spittini <spittini@gmail.com>
2024-12-16 15:53:43 -0500

    v1.2 - vs structure adjusted

---
+++ Tests/MainTest.cs
@@ -0,0 +1,22 @@
+﻿public class MainTest
+{
+    public static void Execute()
+    {
+        var matrix = CreateMatrix();
+        var wordstream = CreateWordstream();
+
+        WordFinder wf = new WordFinder(matrix);
+        wf.ThreadingMode = ThreadingMode.Synchronous; // Synchronous, Task (TPL), Thread
+        wf.ThreadCount = 3; // Number of tasks/threads (1 - 10)
+
+        var result = wf.Find(wordstream);
+
+        foreach (var top in result)
+            Console.WriteLine($"{top}");
+
+        List<string> CreateMatrix() => "abcdc,fgwio,chill,pqnsd,uvdxy,chill,chill".Split(',').ToList<string>();
+        List<string> CreateWordstream() => "cold,wind,snow,chill,wind".Split(',').ToList<string>();
+
+        Console.ReadLine();
+    }
+}
